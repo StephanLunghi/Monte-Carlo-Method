0abae1ae68241b5a5b3f7b8faa30e48c845ffb40
Stephan Lunghi <slunghi43@gmail.com>
2019-04-01 17:52:00 -0400

    Going back and forth with what I want to do. Trying to stay as close to the instructions as possible

--- Monte Carlo Method/Program.cs
+++ Monte Carlo Method/Program.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace Monte_Carlo_Method
 {
+
+
+    ////1.	Create a struct to hold x, y coordinates as doubles. 
+    //Provide a 2-parameter constructor which takes a new set of 
+    //    coordinates and a 1-parameter constructor which takes a 
+    //    Random object and uses the NextDouble method to initialize X and Y.
+
+    ////2.	Create a function which takes an x, y coordinate struct and 
+    //returns a double corresponding to the hypotenuse of a triangle with sides 
+    //    of lengths x, y.
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter something");
+            double something = double.Parse(Console.ReadLine());
+            
+
         }
     }
 }
---
+++ Monte Carlo Method/Struct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monte_Carlo_Method
+{
+    public struct coordinates
+    {
+        double x;
+        double y;
+
+        public coordinates(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public coordinates(Random rando)
+        {
+            this.x = rando.NextDouble();
+            this.y = rando.NextDouble();
+        }
+        public double Hypot()
+        {
+            return Math.Sqrt(Math.Pow(this.x, 2) + Math.Pow(this.y, 2));
+        }
+}
+}   
+
